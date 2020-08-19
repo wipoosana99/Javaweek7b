ba91ec705789db1f45a9467793ed9b1b55893039
wipoosana99 <50567068+wipoosana99@users.noreply.github.com>
2020-08-19 12:04:53 +0700

    Add files via upload

---
+++ myJava701.java
@@ -0,0 +1,17 @@
+package week7b;
+
+public class myJava701 {
+	public static void main(String[]args) {
+		char text1 ='M';
+		char text2 ='A';
+		char aStart = '*';
+		char text3 = 'B';
+		char text4 = 'D';
+		System.out.println("Hellow\n ther");
+		System.out.println("Text1 \b" + text1);
+		System.out.println("Text2 \t" + text2);
+		System.out.println("Text3 \r" + aStart);
+		System.out.println("Text3 \\" + text3);
+		System.out.println("Text4\''" + text4);
+	}
+}
---
+++ myJava702.java
@@ -0,0 +1,22 @@
+package week7b;
+
+public class myJava702 {
+	public static void main(String[]args) {
+		int a=5;
+		int b=10;
+		int c=20;
+		int d=35;
+		int total = a+b;
+		int total2 = b/a;
+		int total3 = a*c;
+		int total4 = a%c;
+		int total5 = d%b;
+		float total6 = d*a/b-c;
+		System.out.println("Total = " + total);
+		System.out.println("Total2 = " + total2);
+		System.out.println("Total3 = " + total3);
+		System.out.println("Total4 = " + total4);
+		System.out.println("Total5 = " + total5);
+		System.out.println("Total6 = %2.f " + total6);
+	}
+}
